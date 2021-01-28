984609483e1618332f5b525271c57d27d8d4292b
Yash Jungade <60670792+yashjungade@users.noreply.github.com>
2021-01-28 13:40:06 +0530

    Add files via upload

---
+++ MULTIMODE_CAR.ino
@@ -0,0 +1,151 @@
+/* define section */
+
+//Motor driver setup L298N
+const int IN1 = 7; //connect IN1 in the motor driver to pin number 7 in arduino
+const int IN2 = 6; //connect IN2 in the motor driver to pin number 6 in arduino
+const int IN3 = 5; //connect IN3 in the motor driver to pin number 5 in arduino
+const int IN4 = 4; //connect IN4 in the motor driver to pin number 4 in arduino
+
+/* this will allow you to control the speed of the motors */
+
+const int ENA = 9; //connect IN1 in the motor driver to pin number 9 in arduino
+const int ENB = 3; //connect IN1 in the motor driver to pin number 3 in arduino
+
+//***************************************************************************//
+
+int command = 0;// int to store the data that we will receive from the bluetooth
+
+/* define the pins for the ultrasonic sensor */
+
+int trig = 11; /* connect trigger pin of Ultrasonic to pin number 11 */
+int echo = 12; /* connect echo pin of Ultrasonic to pin number 12 */
+
+/* void setup section we are going to setup our pins to input or output */
+
+void setup() {
+  Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+  
+  // set motor driver`s pins to output
+  pinMode (IN1, OUTPUT);
+  pinMode (IN2, OUTPUT);
+  pinMode (IN3, OUTPUT);
+  pinMode (IN4, OUTPUT);
+  pinMode (ENA, OUTPUT);
+  pinMode (ENB, OUTPUT);
+//set trig pin as output and the echo pin as input for the ultrasonic sesor
+  pinMode(trig, OUTPUT);
+pinMode(echo, INPUT);
+}
+
+/*void loop section we are going to write our main program here*/
+
+void loop()
+{
+
+
+  if (Serial.available() > 0) //check if the arduino is receiving data or not
+{
+  command=Serial.read(); //set command integer value sama as the value that we will receive from the bluetooth NOT(we are going to send characters from 0 to 5 and each character will do a different job.
+
+}
+if (command == '0')//if we receive a character 0 then both motors will move forward
+
+  {
+    // by setting IN1 and IN3 HIGH and IN2 and IN4 LOW both motors will move forward accoring to my connections and ENA and ENB to control the speed from 0 to 250 and this same as the following characters
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    analogWrite(ENA, 255);
+    analogWrite(ENB, 255);
+command=0; //set command value back to 0
+  }
+else if(command == '1') // moving backward
+  {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENA, 255);
+    analogWrite(ENB, 255);
+    
+    command=0; //set command value back to 0
+  }
+ else if (command == '2') //moving left
+ {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+analogWrite(ENA, 255);
+    analogWrite(ENB, 255);
+      command=0; //set command value back to 0
+  }
+else if (command == '3') //moving right
+{
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+analogWrite(ENA, 255);
+    analogWrite(ENB, 255);
+      command=0; //set command value back to 0
+  }
+else if (command == '4') //stop the motors
+{
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, HIGH);
+      command=0; //set command value back to 0
+  }
+else if (command == '5') 
+{
+
+
+long du = 0, ds = 0; // Transmitting pulse
+digitalWrite(trig, LOW); //set trig pin to high
+delayMicroseconds(2); //wait 2MS
+digitalWrite(trig, HIGH); //set trig pin to high to send pulse
+delayMicroseconds(10); //wait 10MS
+digitalWrite(trig, LOW);// Waiting for pulse
+du = pulseIn(echo, HIGH);// check for the pulse
+ds = du*0.034/2 ;// Calculating distance
+
+//move the motors forward
+
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    analogWrite(ENA, 255);
+    analogWrite(ENB, 255);
+
+if (ds<=20) //if the space betwen the robot and the object less than 10 or 10 then turn right until the robot gets a space more than 10
+{
+    analogWrite(ENA, 255);
+    analogWrite(ENB, 255);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    delay(1000);
+} 
+else
+{
+    analogWrite(ENA, 255);
+    analogWrite(ENB, 255);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    delay(1000);
+} 
+command='5'; //here we will let the robot do the same job untill the we press stop bottom from mobile or press any bottom so we control it manualy 
+}
+
+
+}
+
+
+// created by abdulrahman rijabo :)
---
+++ Thermal_Screening.ino
@@ -0,0 +1,29 @@
+//Thermal Screening System by Yash Jungade, Kartik Bodhankar, Rugved Kharbade and Sumant Puranik.
+#include <Wire.h>
+#include <Adafruit_MLX90614.h>
+#include <LiquidCrystal.h>
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+LiquidCrystal lcd(12,11,5,4,3,2);
+void setup() {
+  mlx.begin();
+  Serial.begin(9600);
+  Serial.println("Thermal Screener");
+  lcd.begin(16,2);
+  lcd.print("Thermal Screener");
+}
+void loop() {
+    Serial.print("Temperature : ");Serial.print(mlx.readObjectTempC());Serial.println(" C");
+    lcd.print("Thermal Screener");
+    delay(1000);
+    lcd.clear();
+    lcd.print("Temp : ");lcd.print(mlx.readObjectTempC());lcd.print(" C");
+    lcd.setCursor(0,1);
+    if(mlx.readObjectTempC()<=38){
+      lcd.print("Safe");
+    }
+    else{
+      lcd.print("UnSafe");
+    }
+    delay(5000);
+    lcd.clear();
+}
